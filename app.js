1888b1df2823bd13627813b940d92993338aec70
Angad Sandhu <angadsandhu@Angads-MacBook-Air.local>
2023-05-01 17:11:26 +0530

    01May23: Added Package JSON

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const fs = require("fs");
 
-fs.writeFileSync("Demo.text", "My first note using Node JS");
+fs.writeFileSync("Demo.txt", "My first note using Node JS");
 // writeFileSync is a sync method used for writing output to a file
+
+fs.appendFileSync("Demo.txt", "\n Developer : Angad");
---
+++ notesUtils.js
@@ -0,0 +1,5 @@
+const getNotes = function(){
+    return "My notes for you ..";
+}
+
+module.exports  = getNotes;

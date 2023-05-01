af7a9b49cf7250d4d5328dee8402782114294c4e
Angad Sandhu <angadsandhu@Angads-MacBook-Air.local>
2023-05-01 19:25:58 +0530

    01May23: Added Chalk

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 const validator  = require("validator");
+const chalk = require("chalk");
 const getNotes = require("./utils/notesUtils");
 
+const blueText = function(text) {
+    return chalk.blue(text);
+}
+
+const redText = function(text) {
+    return chalk.red(text);
+}
+
+
 console.log(getNotes());
+console.log(validator.isEmail("abc@abc")? blueText("Valid Email !!"): redText("WRONGO!"));

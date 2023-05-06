5bdbdac914e15c2284bcfda64f9c66fb26fbb1f8
AngadSandhu <angad.sandhu16@gmail.com>
2023-05-06 18:59:40 +0530

    06May2023: Added Builder Config to custom commands

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const yargs = require("yargs");
 const chalk = require("chalk");
 const getNotes = require("./utils/notesUtils.js");
+const { string } = require("yargs");
 
 const commandArg = process.argv[2];
 
@@ -12,8 +13,19 @@ console.log(chalk.blue.bold(commandArg));
 yargs.command({
     command: "add",
     describe: "command for adding a new note",
-    handler: function() {
-        console.log("Adding a note..");
+    builder: {                       // builder config is used for adding the custom command
+        title: {                     // name of the custom property/command that user gives during runtime 
+            describe: "note title",  // description of the custom command arg during --help
+            demandOption: true,       // wether the ccommand is required at runtime or not
+            type: string
+        },
+        content: {
+            describe:  "note content",
+            demandOption: false
+        }
+    },
+    handler: function(argv) {
+        console.log("Adding a note..",argv);
     }
 });
 
@@ -33,11 +45,13 @@ yargs.command({
     }
 })
 
-yargs.comand({
+yargs.command({
     command: "read",
     describe: "command for reading notes",
     handler: function(){
         console.log("Reading the note");
     }
 })
-console.log("Yargs Arguments", yargs.argv);
+
+yargs.parse();  // This is REQUIRED to parse all the custom arguments being passed down by the user 
+//console.log("Yargs Arguments", yargs.argv);
